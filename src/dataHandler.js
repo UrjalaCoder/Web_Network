3454658f36383130bdf3b355d098e488fbbd82d8
Tuomas Myllymaki <myllymakituomas1998@gmail.com>
2019-08-03 19:43:28 +0300

    Add: procfile

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -3,7 +3,7 @@ import path from 'path';
 
 function init(data) {
     const { spawn } = child_process;
-    const pythonFile = path.join(__dirname, '/venv/Scripts/python.exe');
+    const pythonFile = 'python.exe';
     const scriptFile = path.join(__dirname, 'network_scripts', 'main.py');
 
     // console.log(pythonFile);
